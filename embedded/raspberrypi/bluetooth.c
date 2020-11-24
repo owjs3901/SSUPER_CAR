98a952edbde2b6613190f7862357b55c2ef54ec8
shimhm <55525917+shimhm@users.noreply.github.com>
2020-11-25 05:02:57 +0900

    Add files via upload
    bluetooth 모듈 c언어

--- embedded/raspberrypi/bluetooth.c
+++ embedded/raspberrypi/bluetooth.c
@@ -1,45 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include <bluetooth/rfcomm.h>
 
 int main(int argc, char **argv)
 {
-    inquiry_info *ii = NULL;
-    int max_rsp, num_rsp;
-    int dev_id, sock, len, flags;
-    int i;
-    char addr[19] = { 0 };
-    char name[248] = { 0 };
+  struct sockaddr_rc addr = { 0 };
+  int s, status;
+  char dest[18] = "98:D3:71:FD:BA:62";
 
-    dev_id = hci_get_route(NULL);
-    sock = hci_open_dev( dev_id );
-    if (dev_id < 0 || sock < 0) {
-        perror("opening socket");
-        exit(1);
-    }
+  // allocate a socket
+  s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    len  = 8;
-    max_rsp = 255;
-    flags = IREQ_CACHE_FLUSH;
-    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
-    
-    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
-    if( num_rsp < 0 ) perror("hci_inquiry");
+  // set the connection parameters (who to connect to)
+  addr.rc_family = AF_BLUETOOTH;
+  addr.rc_channel = (uint8_t) 1;
+  str2ba( dest, &addr.rc_bdaddr );
 
-    for (i = 0; i < num_rsp; i++) {
-        ba2str(&(ii+i)->bdaddr, addr);
-        memset(name, 0, sizeof(name));
-        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
-            name, 0) < 0)
-        strcpy(name, "[unknown]");
-        printf("%s  %s\n", addr, name);
-    }
+  // connect to server
+  status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
-    free( ii );
-    close( sock );
-    return 0;
+  printf("%d\n",status);
+
+  // send a message
+  if( status == 0 ) {
+       status = write(s, "1", 2);
+  }
+
+  if( status < 0 ) perror("uh oh");
+
+  close(s);
+  return 0;
 }
